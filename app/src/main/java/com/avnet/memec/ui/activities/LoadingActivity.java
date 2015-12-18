e21bd33ee70ac199c3f402d486811feb07d4f88c
karamnj <karamnj@gmail.com>
2015-12-18 19:38:43 +0530

    Initial Commit

--- app/src/main/java/com/avnet/memec/ui/activities/LoadingActivity.java
+++ app/src/main/java/com/avnet/memec/ui/activities/LoadingActivity.java
@@ -1,12 +1,9 @@
 package com.avnet.memec.ui.activities;
 
 import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.ProgressBar;
 
 import com.avnet.memec.R;
@@ -28,28 +25,8 @@ public class LoadingActivity extends BaseActivity {
         Log.d(CLASS_NAME, "Set current object");
         bar = (ProgressBar) findViewById(R.id.loading_progress);
         settings = PreferenceManager.getDefaultSharedPreferences(this);
-        bar.setProgress(50);
-    }
+        bar.setProgress(90);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
